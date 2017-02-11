59fcc825086a51a750d509bc7ca97190fd90b0d2
mathee <sivanm2@mcmaster.ca>
2017-02-11 17:18:53 -0500

    needs enforcing

--- Makefile
+++ Makefile
@@ -1,16 +1,22 @@
 CC=g++
 CCFLAGS=-g -Wall -fopenmp 
-OBJS= pa2.o
+OBJS= pa2.o verify.o valinput.o
 
-all: pa2.x 
+all: pa2.x
 
 pa2.x: $(OBJS) new
 	$(CC) $(CCFLAGS) $(OBJS) -o pa2.x 
 
 pa2.o: pa2.cpp 
-	$(CC) $(CCFLAGS) -c pa2.cpp -fopenmp
+	$(CC) $(CCFLAGS) -c pa2.cpp
+
+valinput.o: valinput.cpp
+	$(CC) $(CCFLAGS) -c valinput.cpp
+
+verify.o: verify.cpp
+	$(CC) $(CCFLAGS) -c verify.cpp
 
 new:
 
 clean:
-	rm *.o *.x
+	rm *.o *.x *.txt
--- pa2.cpp
+++ pa2.cpp
@@ -7,41 +7,247 @@
 #include <omp.h>
 #include <time.h>
 #include <unistd.h>
+#include <cstdlib>
+#include <fstream>
+
+#include "verify.h"
+#include "valinput.h"
 
 using namespace std;
 
-string alphabet = {"abcdefgh"};
 string S;
-int stringLength;
+omp_lock_t mutex;
+int counter = 0;
+int currStringLength = 0;
+int totalStringLength;
+int numOfSegmentsSatisfied = 0;
+bool stringLengthMaxed = false;
+int occurOfC0, occurOfC1, occurOfC2;
+char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
 
-void update(char letter, int my_rank) {
+struct INPUT {
+	int F;
+	int N;
+	int L;
+	char c0, c1, c2;
+};
+INPUT inSt;
+
+void writeOutputFile() {
+   ofstream textFile;
+   textFile.open("out.txt");
+   textFile << S << endl;
+   textFile << numOfSegmentsSatisfied << endl;
+   textFile.close();
+}
+
+void update(char letter, int thread_id) {
    S += letter;
-   stringLength++; 
-   printf("THREAD %i APPENDED LETTER: %c - SEGMENT S: %s %i \n", my_rank, letter, S.c_str(), stringLength);
+   currStringLength++;
+
+   if (currStringLength == totalStringLength) {
+      stringLengthMaxed = true; 
+   }
+   
+   printf("THREAD %i APPENDED LETTER: %c - SEGMENT S: %s %i \n", thread_id, letter, S.c_str(), currStringLength);
 }
 
 void constructS() {
-   int my_rank = omp_get_thread_num();
-   const char letter = alphabet[my_rank];
+   int thread_id = omp_get_thread_num();
+   const char letter = alphabet[thread_id];
 
-   srand(time(NULL));
-   unsigned int microsleep = rand()%500+100;
-   usleep(microsleep);
+   while (stringLengthMaxed == false) {
+      unsigned int microsleep = rand() % 500 + 100;
+      printf("THREAD %i PUT TO SLEEP FOR: %i\n", thread_id, microsleep);
+      usleep(microsleep);
 
-   #pragma omp critical
-   update(letter,my_rank);
+      omp_set_lock(&mutex);
+      if (!stringLengthMaxed) {
+         #pragma omp critical
+         update(letter,thread_id);
+      }
+      omp_unset_lock(&mutex);
+   }
+}
 
+void checkSegmentProp() {
+   int thread_id = omp_get_thread_num();
+   int start = thread_id * inSt.L + counter * inSt.L * inSt.N;
+   
+   string strSeg = S.substr(start, inSt.L);
+
+   omp_set_lock(&mutex);
+   if (verifyF(inSt.F, inSt.c0, inSt.c1, inSt.c2, strSeg) == true) {
+      numOfSegmentsSatisfied++;
+   }
+   omp_unset_lock(&mutex);
+
+   printf("-----THREAD: %i Substr: %s   Satisfied: %i\n", thread_id, strSeg.c_str(), numOfSegmentsSatisfied); 
+
+}
+
+char chooseChar(int ranA, int ranB, int ranC) {
+   char chosenChar;
+
+   switch (inSt.F) {
+      case 0:
+      if ((S[currStringLength]=='\0')&&(currStringLength%inSt.L==0)) {
+         chosenChar=inSt.c2; occurOfC2++; 
+      } else if (occurOfC0 + occurOfC1 < occurOfC2) {
+         if (ranC==1) {
+            chosenChar=inSt.c0; occurOfC0++;
+         } else if (ranC==2) {
+            chosenChar=inSt.c1; occurOfC1++; 
+         }
+      } else if (((occurOfC0 + occurOfC1 >= occurOfC2)&&(inSt.N<4)) || (occurOfC0 + occurOfC1 > occurOfC2)) {
+         chosenChar=inSt.c2; occurOfC2++;
+      }
+      break;
+      
+      case 1:
+      if ((S[currStringLength]=='\0')&&(currStringLength%inSt.L==0)) {
+            chosenChar=inSt.c1; occurOfC1++; 
+      } else if (occurOfC0 + 2*occurOfC1 < occurOfC2) {
+         if (occurOfC0 + 2*occurOfC1 < occurOfC2+1) {
+            chosenChar=inSt.c0; occurOfC0++;}
+         else if (occurOfC0 + 2*occurOfC1 < occurOfC2+2){
+            chosenChar=inSt.c1; occurOfC1++;}
+      } else if (((occurOfC0 + 2*occurOfC1 >= occurOfC2)&&(inSt.N<4)) ||(occurOfC0 + 2*occurOfC1 > occurOfC2)) {
+         chosenChar=inSt.c2; occurOfC2++;
+      }
+      break;
+
+      case 2:
+      if ((S[currStringLength]=='\0')&&(currStringLength%inSt.L==0)) {
+         chosenChar=inSt.c2; occurOfC2++; 
+      } else if (occurOfC0 * occurOfC1 < occurOfC2) {
+         if ((occurOfC0+1) * occurOfC1 == occurOfC2) {
+            chosenChar=inSt.c0; occurOfC0++;
+         } else if ((occurOfC0 * (occurOfC1+1) == occurOfC2) || ((occurOfC0+1) * (occurOfC1+1) == occurOfC2)) {
+            chosenChar=inSt.c1; occurOfC1++; }
+      } else if (((occurOfC0 * occurOfC1 >= occurOfC2)&&(inSt.N<4)) || (occurOfC0 * occurOfC1 > occurOfC2)) {
+         chosenChar=inSt.c2; occurOfC2++;
+      }
+      break;
+      
+      case 3:
+      if ((S[currStringLength]=='\0')&&(currStringLength%inSt.L==0)) {
+            chosenChar=inSt.c1; occurOfC1++; 
+      } else if (occurOfC0 - occurOfC1 < occurOfC2) {
+         if (occurOfC0+1 - occurOfC1 < occurOfC2) {
+            chosenChar=inSt.c1; occurOfC1++;
+         } else if (occurOfC0 - occurOfC1 < occurOfC2+1) {
+            chosenChar=inSt.c0; occurOfC0++; }
+      } else if (((occurOfC0 - occurOfC1 >= occurOfC2)&&(inSt.N<4)) || (occurOfC0 - occurOfC1 > occurOfC2)) {
+         chosenChar=inSt.c2; occurOfC2++;
+      }
+      break;
+   }
+
+   if (chosenChar=='\0') {       
+      chosenChar=alphabet[ranA-1];    
+   }    
+
+   return chosenChar; 
+}
+
+void constructSE() {
+   int thread_id = omp_get_thread_num();
+
+   //unsigned int distrA = rand() % 500 + 100;
+   //uniform_int_distribution<> distrA(4, ((inSt.N<4) ? 4 : inSt.N) );
+   //uniform_int_distribution<> distrC(1, 2);
+
+   while (stringLengthMaxed == false) {
+      unsigned int microsleep = rand() % 500 + 100;
+      printf("THREAD PUT TO SLEEP FOR: %i\n", microsleep);
+      usleep(microsleep);
+
+      omp_set_lock(&mutex);
+      if (!stringLengthMaxed) {
+         char letter = chooseChar(4, 154, 2);
+         currStringLength++;
+         S += letter;
+         printf("THREAD %i APPENDED LETTER: %c - SEGMENT S: %s %i \n", thread_id, letter, S.c_str(), currStringLength);
+      }
+
+      if (currStringLength == totalStringLength) {
+         stringLengthMaxed = true; 
+      }
+      omp_unset_lock(&mutex);
+   }
 }
 
 int main(int argc, char* argv[]) {
    printf("\n---------------PROGRAM START: \n");
 
-   int thread_count = strtol(argv[1], NULL, 10);
+   inSt.F = strtol(argv[1], NULL, 10);
+   int N = strtol(argv[2], NULL, 10);
+   int L = strtol(argv[3], NULL, 10);
+   int M = strtol(argv[4], NULL, 10);
+   inSt.c0 = argv[5][0];
+   inSt.c1 = argv[6][0]; 
+   inSt.c2 = argv[7][0];
 
-   #pragma omp parallel num_threads(thread_count)
+   totalStringLength = M * L;
+
+   if (!validateInput(inSt.F, N, L, M)) {
+   	printf("Exiting program...\n");
+   	return 0;
+   }
+
+   // generates a seed of pseudo-random numbers
+   srand(time(0));
+   #pragma omp parallel num_threads(N)
    constructS();
 
-   printf("--------String: %s\n", S.c_str());
+   printf("\n---------------String S CREATED: %s\n", S.c_str());
+
+   inSt.L = L;
+   inSt.N = N;
+   omp_init_lock(&mutex);
+   while (counter < M/N) {
+      #pragma omp parallel num_threads(N)
+      checkSegmentProp();
+
+      counter++;
+      if ((counter==M/N) && (M%N!=0)) {
+         #pragma omp parallel for
+         for (int i=0; i < M%N; i++){
+            checkSegmentProp();
+         }
+      }
+   }
+   writeOutputFile();
+
+   //ENFORCING S
+   printf("\n---------------ENFORCING S: \n");
+   S = "";
+   counter = 0;
+   currStringLength = 0;
+   stringLengthMaxed = false;
+   numOfSegmentsSatisfied = 0;
+   occurOfC0=0; occurOfC1=0; occurOfC2=0;
+
+   #pragma omp parallel num_threads(N)
+   constructSE();
+
+   printf("\n---------------String S CREATED: %s\n", S.c_str());
+
+   while (counter < M/N) {
+      #pragma omp parallel num_threads(N)
+      checkSegmentProp();
+
+      counter++;
+      if ((counter==M/N) && (M%N!=0)) {
+         #pragma omp parallel for
+         for (int i=0; i < M%N; i++) {
+            checkSegmentProp();
+         }
+      }
+   }
+   counter = 0;
+   writeOutputFile();
 
    return 0;
 }
